dff5538a4300d8c80a3b67437439888bf828bda7
mucki024 <mucki024.0@gmail.com>
2021-10-29 16:13:28 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MonsterCardGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
